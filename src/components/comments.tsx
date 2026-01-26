bf6f1819fc8d52bcfb357fcb8a4e8b09043d2798
khil94 <zidru94@gmail.com>
2026-01-26 15:58:09 +0900

    fix : Comment 수정
    - 카테고리 이름 불일치하던것 수정
    - 테마 색상 거꾸로 적용되던 것 수정

--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -9,15 +9,15 @@ export default function Comments() {
     <div className="mt-10">
       <Giscus
         id="comments"
-        repo="khil94/blog" // 예: "gildong/my-blog"
-        repoId="R_kgDOQ0AE3w" // Giscus 사이트에서 얻은 값
-        category="General" // 보통 General이나 Comments 선택
-        categoryId="DIC_kwDOQ0AE384C1ZjG" // Giscus 사이트에서 얻은 값
-        mapping="pathname" // 페이지 경로(URL)를 기준으로 댓글 분류
+        repo="khil94/blog"
+        repoId="R_kgDOQ0AE3w"
+        category="Announcements"
+        categoryId="DIC_kwDOQ0AE384C1ZjG"
+        mapping="pathname"
         reactionsEnabled="1" // 이모지 반응 켜기
         emitMetadata="0"
         inputPosition="bottom"
-        theme={theme === "dark" ? "light" : "dark"}
+        theme={theme === "dark" ? "dark" : "light"}
         lang="ko"
         loading="lazy"
       />
